ddccf079cfdbd055e5989c647d635e3e83f0df1e
chernyshev-alexander <achernyshev@griddynamics.com>
2022-07-27 19:19:16 +0200

    fixed AddBook

--- internal/rest/books.go
+++ internal/rest/books.go
@@ -11,23 +11,6 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
-/*
-//go:generate go run github.com/deepmap/oapi-codegen/cmd/oapi-codegen --config=conf/server-gen.conf  ../../books.yaml
-//go:generate counterfeiter -generate
-//counterfeiter:generate -o test/books-service.gen.go . BookService
-*/
-
-/*
-//go:generate go run github.com/deepmap/oapi-codegen/cmd/oapi-codegen --config=conf/client-gen.conf  ../../books.yaml
-//go:generate go run github.com/deepmap/oapi-codegen/cmd/oapi-codegen --config=conf/types-gen.conf   ../../books.yaml
-*/
-
-// type BooksService interface {
-// 	AddBook(context.Context, AddBookRequest) (Book, error)
-// 	DeleteBook(context.Context, int) error
-// 	SearchBooksByAuthor(context.Context, SearchBooksByAuthorParams) ([]Book, error)
-// }
-
 type BookHandler struct {
 	booksSpi service.BooksService
 }
@@ -38,26 +21,37 @@ func NewBooksHandler(spi service.BooksService) *BookHandler {
 	return &BookHandler{booksSpi: spi}
 }
 
-// AddBook implements ServerInterface
+// -- ServerInterface --
+
 func (h *BookHandler) AddBook(c *gin.Context) {
 	var book gen.Book
+
 	if err := json.NewDecoder(c.Request.Body).Decode(&book); err != nil {
 		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
 		return
 	}
 	defer c.Request.Body.Close()
 
-	responseJson(c.Writer, http.StatusOK, book)
+	b, err := h.booksSpi.AddBook(c.Request.Context(), book)
+	if err != nil {
+		http.Error(c.Writer, err.Error(), http.StatusNotFound)
+		return
+	}
+	responseJson(c.Writer, http.StatusOK, b)
 }
 
 func (h *BookHandler) DeleteBook(c *gin.Context, bookId int) {
-	h.booksSpi.DeleteBook(c.Request.Context(), bookId)
+	if err := h.booksSpi.DeleteBook(c.Request.Context(), bookId); err != nil {
+		http.Error(c.Writer, err.Error(), http.StatusNotFound)
+		return
+	}
+	responseJson(c.Writer, http.StatusOK, nil)
 }
 
 func (h *BookHandler) SearchBooksByAuthor(c *gin.Context, params gen.SearchBooksByAuthorParams) {
 	books, err := h.booksSpi.SearchBooksByAuthor(c.Request.Context(), params)
 	if err != nil {
-		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		http.Error(c.Writer, err.Error(), http.StatusNotFound)
 		return
 	}
 	responseJson(c.Writer, http.StatusOK, books)
